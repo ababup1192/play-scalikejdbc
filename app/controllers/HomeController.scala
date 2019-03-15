cc7f3e3df5cd509917b98a44763b44f2c20d6593
abab <ababup1192@gmail.com>
2019-03-16 08:19:09 +0900

    作ったIDで検索

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -7,21 +7,23 @@ import scalikejdbc._
 case class User(id: Long, name: String)
 
 object User extends SQLSyntaxSupport[User] {
-  private[this] val u = User.syntax("u")
-
   override val schemaName: Option[String] = None
   override val tableName: String = "users"
   override lazy val columns: Seq[String] = Seq("id", "name")
 
   def apply(rn: ResultName[User])(rs: WrappedResultSet): User = autoConstruct(rs, rn)
+}
+
+object UserDao {
+  private[this] val u = User.syntax("u")
 
   def create(name: String)(implicit s: DBSession = AutoSession): Long =
     withSQL {
       insertInto(User).values(null, name)
     }.updateAndReturnGeneratedKey().apply()
 
-  def findByName(name: String)(implicit s: DBSession = AutoSession): Option[User] = withSQL {
-    select.from(User as u).where.eq(u.name, name)
+  def findById(id: Long)(implicit s: DBSession = AutoSession): Option[User] = withSQL {
+    select.from(User as u).where.eq(u.id, id)
   }.map(User(u.resultName)).single().apply()
 }
 
@@ -41,8 +43,8 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
     }
 
     val user = DB.localTx { implicit session =>
-      User.create("John")
-      User.findByName("John")
+      val id = UserDao.create("John")
+      UserDao.findById(id)
     }
 
     Ok(user.toString)
