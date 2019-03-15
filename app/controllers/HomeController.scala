c1583dbd9d0ae76c9c2c15121f7f0b830619137d
abab <ababup1192@gmail.com>
2019-03-16 07:18:41 +0900

    DSLで型安全に

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,42 +3,47 @@ package controllers
 import javax.inject._
 import play.api.mvc._
 import scalikejdbc._
+import scalikejdbc.config.DBs
 
-case class User(id: Long, name: String)
+case class User(name: String)
 
 object User extends SQLSyntaxSupport[User] {
 
   override val schemaName: Option[String] = None
   override val tableName: String = "users"
 
-  def apply(u: ResultName[User])(rs: WrappedResultSet): User = {
-    User(rs.long(u.id), rs.string(u.name))
-  }
+  override def columnNames: Seq[String] = Seq("name")
+
+  def apply(rs: WrappedResultSet) = new User(
+    rs.string("name")
+  )
 }
 
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
-  implicit val session = AutoSession
+  implicit val session: AutoSession.type = AutoSession
 
-  /**
-    * Create an Action to render an HTML page.
-    *
-    * The configuration in the `routes` file means that this method
-    * will be called when the application receives a `GET` request with
-    * a path of `/`.
-    */
   def index() = Action { implicit request: Request[AnyContent] => {
+    val u = User.syntax("u")
     val name = "John"
 
-    val user =
-      try sql"select * from users".toMap.single.apply()
-      catch {
-        case e: Exception =>
-          sql"create table users(name varchar(100) not null)".execute.apply()
-          sql"insert into users values ($name)".update.apply()
-          sql"select * from users".toMap.single().apply()
-      }
+    sql"""
+    drop table if exists users;
+
+    create table users(
+      name varchar(64)
+    )
+    """.execute.apply()
+
+    withSQL {
+      insertInto(User).values(name)
+    }.execute().apply()
+
+    val user = withSQL {
+      select.from(User as u)
+    }.map(rs => User(rs)).single().apply()
 
     Ok(user.toString)
-  }}
+  }
+  }
 }
